0ba405a259d110c466f794b85a7c1f82cc37034b
4rtsy2wo <120429109+4rtsy2wo@users.noreply.github.com>
2023-12-21 19:09:20 +0100

    progress bar, error handling, GUI, refactor

--- ytDL.py
+++ ytDL.py
@@ -1,46 +1,96 @@
 import os
+import pathlib
 import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import filedialog, messagebox, ttk
 from pytube import YouTube
+from pytube.exceptions import PytubeError, VideoUnavailable, AgeRestrictedError, MembersOnly, VideoPrivate, VideoRegionBlocked, LiveStreamError, RecordingUnavailable, MaxRetriesExceeded, HTMLParseError, ExtractError, RegexMatchError
 
+# version number
+version = "0.2.2"
+# Set the main_path to a subfolder within the user's home directory
+main_path = os.path.join(os.path.expanduser('~'), 'YT_Downloader')
+# Dictionary to map media choices to sub directories
+sub_dirs = {'a': 'Audio_Only', 'v': 'Video_Only', 'c': 'Video_with_Audio'}
 
-# Set the main_path to a subfolder within the user's Videos folder
-main_path = os.path.join(os.path.expanduser('~'), 'Videos', 'YT_Downloads')
+# Dictionary to map media choices to filter functions
+filter_functions = {
+    'a': lambda streams: streams.filter(only_audio=True).order_by('abr').desc(),
+    'v': lambda streams: streams.filter(only_video=True).order_by('resolution').desc(),
+    'c': lambda streams: streams.filter(progressive=True).order_by('resolution').desc()
+}
 
+# get streams from url
 def fetch_streams():
-    global streams
     clear_stream_buttons()
     video_url = url_entry.get()
     media_choice = media_var.get().lower()
-    yt = YouTube(video_url)
-    streams = yt.streams
-    if media_choice == 'a':
-        streams = streams.filter(only_audio=True).order_by('abr').desc()
-    elif media_choice == 'v':
-        streams = streams.filter(only_video=True).order_by('resolution').desc()
-    else:
-        streams = streams.filter(progressive=True).order_by('resolution').desc()
+    try:
+        yt = YouTube(video_url, on_progress_callback=download_progress, allow_oauth_cache=False, use_oauth=False)
+        streams = yt.streams
+        streams = filter_functions[media_choice](streams)
+        for stream in streams:
+            stream_info = get_stream_info(stream, media_choice)
+            stream_button = create_stream_button(stream_info, stream)
+            stream_button.pack(fill='x')
+            stream_buttons.append(stream_button)
+    except PytubeError as e:
+        handle_error(e)
 
-    for stream in streams:
-        if media_choice == 'a':
-            stream_info = f"{stream.abr} ({stream.mime_type} - Codec: {stream.audio_codec.split('.')[0]})"
-        else:
-            stream_info = f"{stream.resolution} ({stream.mime_type} - {stream.fps} fps - Codec: {stream.video_codec.split('.')[0]})"
-        stream_button = tk.Button(scrollable_frame, text=stream_info, fg='yellow', bg='black', command=lambda s=stream: download_media(s))
-        stream_button.pack(fill='x')
-        stream_buttons.append(stream_button)
+# error handling
+def handle_error(e):
+    if isinstance(e, AgeRestrictedError):
+        messagebox.showerror("Age Restricted", f"Sorry, the Video {e.video_id} is age-restricted.")
+    elif isinstance(e, MembersOnly):
+        messagebox.showerror("MembersOnly", f"Members only video, skipping.")
+    elif isinstance(e, VideoPrivate):
+        messagebox.showerror("Video Private", f"Video {e.video_id} is private, skipping.")
+    elif isinstance(e, VideoRegionBlocked):
+        messagebox.showerror("Video Region Blocked", f"Video {e.video_id} is Region blocked, skipping.")
+    elif isinstance(e, VideoUnavailable):
+        messagebox.showerror("Video Unavailable", f"Video {e.video_id} is unavailable, skipping.")
+    elif isinstance(e, LiveStreamError):
+        messagebox.showerror("Live Stream", f"Video {e.video_id} is a Live Stream, skipping.")
+    elif isinstance(e, RecordingUnavailable):
+        messagebox.showerror("Recording Unavailable", f"Recording {e.video_id} is not avaiable, skipping.")
+    elif isinstance(e, MaxRetriesExceeded):
+        messagebox.showerror("Max Retries Exceeded", "An error occurred during download: Maximum number of retries exceeded.")
+    elif isinstance(e, HTMLParseError):
+        messagebox.showerror("HTML Parse Error", "An error occurred during download: HTML could not be parsed.")
+    elif isinstance(e, ExtractError):
+        messagebox.showerror("Extract Error", "An error occurred during download: Data extraction error.")
+    elif isinstance(e, RegexMatchError):
+        messagebox.showerror("Regex Match Error", f"An error occurred during download: {e.caller}: could not find match for {e.pattern}")
+    else:
+        messagebox.showerror("Unknown Error", f"An unknown error occurred during download: {str(e)}")
+        
+#get stream info
+def get_stream_info(stream, media_choice):
+    codec = stream.audio_codec if media_choice == 'a' else stream.video_codec
+    return f"{stream.abr if media_choice == 'a' else stream.resolution} ({stream.mime_type} - {'Codec: ' + codec.split('.')[0] if media_choice == 'a' else str(stream.fps) + ' fps - Codec: ' + codec.split('.')[0]})"
+
+# create stream buttons
+def create_stream_button(stream_info, stream):
+    return tk.Button(scrollable_frame, text=stream_info, fg='yellow', bg='black', command=lambda s=stream: download_media(s))
+
+# sanitize filename and add params
+def get_stream_filename(stream):
+    name, ext = os.path.splitext(stream.default_filename)
+    filename = ''.join(c if c.isalnum() or c in [' ', '_', '-'] else '' for c in name)
+    filename = filename.replace(' ', '_').replace('-', '_')
+    media_type = media_var.get().lower()
+    codec_info = f"{stream.abr}_{stream.audio_codec.split('.')[0]}" if media_type == 'a' else f"{stream.resolution}_{stream.video_codec.split('.')[0]}"
+    return f"{filename}_{codec_info}{ext}"
 
 # download stream
 def download_media(stream):
-    sub_dir = {'a': 'audio', 'v': 'video'}.get(media_var.get().lower(), 'video_audio')
-    output_path = os.path.join(main_path, sub_dir)
+    output_path = os.path.join(main_path, sub_dirs[media_var.get().lower()])
     try:
-        stream.download(output_path=output_path)
+        stream.download(output_path=output_path, filename=get_stream_filename(stream))
         messagebox.showinfo("Download Complete", "Download complete.")
-    except Exception as e:
+    except PytubeError as e:
         messagebox.showerror("Error", f"An error occurred during download: {e}")
-        
-# change download button  
+
+# change download folder
 def select_download_folder():
     folder_selected = filedialog.askdirectory()
     if folder_selected:
@@ -49,82 +99,87 @@ def select_download_folder():
         folder_path_entry.delete(0, tk.END)
         folder_path_entry.insert(0, main_path)
 
-# clean stream buttons when new get fetched
+# delete stream buttons before fetching new ones
 def clear_stream_buttons():
     for button in stream_buttons:
         button.destroy()
     stream_buttons.clear()
-    
+
 # handle mouse scroll
 def on_mousewheel(event):
-    streams_canvas.yview_scroll(int(-1*(event.delta/120)), "units")
-    
+    streams_canvas.yview_scroll(-event.delta//120, "units")
+
+# progress bar
+def reset_progress_bar():
+    progress_bar['value'] = 0
+
+def download_progress(stream, chunk, bytes_remaining):
+    progress_bar['value'] = (1 - (bytes_remaining / stream.filesize)) * 100
+    # update window for progress
+    window.update()
+    # keep the green for 2 secs then reset
+    if bytes_remaining == 0:
+        window.after(2000, reset_progress_bar)
+
+
+## GUI ##
+# create widgets
+def create_pack(widget_type, master, pack_args={}, **kwargs):
+    widget = widget_type(master, **kwargs)
+    widget.pack(**pack_args)
+    return widget
+
 # main window
 window = tk.Tk()
-window.title("YouTube Downloader")
-window.resizable(width=False, height=True)
-
-# URL label
-url_label = tk.Label(window, text="⛓ YouTube Link:", font=('monospace'))
-url_label.pack(side='top', fill='x')
+window.geometry("400x500")
+window.resizable(False, True)
+window.title(f"YouTube Downloader v{version}")
 
 # URL input
-url_entry = tk.Entry(window, font=('monospace', 10))
-url_entry.pack(side='top', fill='x', padx=10, pady=5)
+url_frame = create_pack(tk.Frame, window, pack_args={'side': 'top', 'pady': 5})
+url_label = create_pack(tk.Label, window, text="⛓ YouTube Link:", font=('monospace'), pack_args={'side': 'top', 'fill': 'x'})
+url_entry = create_pack(tk.Entry, window, font=('monospace', 10), pack_args={'side': 'top', 'fill': 'x', 'padx': 10, 'pady': 5})
 
-# folder frame 
-folder_frame = tk.Frame(window)
-folder_frame.pack(side='top', fill='x', expand=True, pady=5, padx=10)
-# download change button
-change_folder_button = tk.Button(folder_frame, text="Change Folder", font=('monospace', 10), command=select_download_folder)
-change_folder_button.pack(side='left')
-# download path
-folder_path_entry = tk.Entry(folder_frame, font=('monospace', 10))
-folder_path_entry.pack(side='left', padx=5, fill='x', expand=True)
-# set initial folder to users video folder
+# media choice
+media_frame = create_pack(tk.Frame, window, pack_args={'side': 'top', 'pady': 10})
+media_label = create_pack(tk.Label, media_frame, text="🎥 Choose Media:", font=('monospace'), pack_args={'side': 'top'})
+media_var = tk.StringVar(value="C")
+media_choices = [("Video with Audio", "C"),("Audio Only", "A"), ("Video Only", "V")]
+for text, choice in media_choices:
+    create_pack(tk.Radiobutton, media_frame, text=text, variable=media_var, value=choice, font=('monospace',), pack_args={'side': 'left'})
+
+# fetch button and instruction label
+instruction_label = create_pack(tk.Label, window, text="|| Retrieve the streams and click to download ||", font=('monospace'))
+fetch_button = create_pack(tk.Button, window, text="Get Streams 🎬", font=('monospace'), bg='red', fg='white', bd=5, relief='solid', command=fetch_streams, pack_args={'side': 'top', 'padx': 5, 'pady': 10})
+
+# progress bar
+progress_frame = create_pack(tk.Frame, window, pack_args={'side': 'bottom', 'fill': 'x', 'expand': True, 'pady': 5, 'padx': 14})
+progress_label = create_pack(tk.Label, progress_frame, text="📥 Progress:", font=('monospace'), pack_args={'side': 'left'})
+progress_bar = create_pack(tk.ttk.Progressbar, progress_frame, orient="horizontal", length=300, mode="determinate", pack_args={'side': 'left'})
+
+# folder frame
+folder_frame = create_pack(tk.Frame, window, pack_args={'side': 'bottom', 'fill': 'x', 'expand': True, 'padx': 10})
+change_folder_button = create_pack(tk.Button, folder_frame, text="Change Folder", font=('monospace', 11), command=select_download_folder, pack_args={'side': 'left'})
+folder_path_entry = create_pack(tk.Entry, folder_frame, font=('monospace', 10), pack_args={'side': 'left', 'padx': 5, 'fill': 'x', 'expand': True})
 folder_path_entry.insert(0, main_path)
 
-# media label
-media_label = tk.Label(window, text="🎥 Choose Media:", font=('monospace'))
-media_label.pack(side='top')
-# media frame for buttons
-media_frame = tk.Frame(window)
-media_frame.pack(side='top')
-# media radio buttons
-media_var = tk.StringVar(value="C")  # Default choice is Audio and Video
-media_choices = [("Video with Audio", "C"), ("Audio Only", "A"), ("Video Only", "V")]
-for text, choice in media_choices:
-    tk.Radiobutton(media_frame, text=text, variable=media_var, value=choice, font=('monospace')).pack(side='left')
-
-# fetch button
-fetch_button = tk.Button(window, text="Get Streams 🎬", font=('monospace'), bg='red', fg='white', bd=5, relief='solid', command=fetch_streams)
-fetch_button.pack(side='top', padx=10, pady=10)
-
-# instruction label
-instruction_label = tk.Label(window, text="📌 Click on a stream when loaded, and wait for the download to finish.", fg='blue', font=('monospace', 10))
-instruction_label.pack()
-
-# stream frame + canvas + scrollbar
-streams_frame = tk.Frame(window)
-streams_frame.pack(side='top', fill='both', expand=True, pady='5')
-
-streams_canvas = tk.Canvas(streams_frame)
-scrollbar = tk.Scrollbar(streams_frame, orient="vertical", command=streams_canvas.yview)
-scrollbar.pack(side="right", fill="y")
-
+# window frame + canvas + scrollbar
+streams_frame = create_pack(tk.Frame, window, pack_args={'side': 'top', 'fill': 'both', 'expand': True, 'pady': 10})
+streams_canvas = create_pack(tk.Canvas, streams_frame)
+scrollbar = create_pack(tk.Scrollbar, streams_frame, orient="vertical", command=streams_canvas.yview, pack_args={'side': "right", 'fill': "y"})
 streams_canvas.pack(side="left", fill="both", expand=True)
 streams_canvas.configure(yscrollcommand=scrollbar.set)
 
-scrollable_frame = tk.Frame(streams_canvas)
+# scrollable frame
+scrollable_frame = create_pack(tk.Frame, streams_canvas)
 streams_canvas.create_window((0, 0), window=scrollable_frame, anchor="nw", width=streams_canvas.winfo_reqwidth())
-
 scrollable_frame.bind("<Configure>", lambda e: streams_canvas.configure(scrollregion=streams_canvas.bbox("all")))
 
-# bind mousewheel 
+# bind mousewheel to canvas
 streams_canvas.bind_all("<MouseWheel>", on_mousewheel)
 
-# stream button array
-stream_buttons = []  # List to keep track of stream buttons
+# stream buttons var
+stream_buttons = []
 
-# start GUI loop
+# run loop
 window.mainloop()
